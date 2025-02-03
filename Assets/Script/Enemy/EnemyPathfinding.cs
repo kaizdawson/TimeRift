208e05a49e10bf4226043bcab65f20abf908ffdc
Kaiz Dawson <kaizdawson@gmail.com>
2025-02-03 21:52:22 +0700

    base

---
+++ Assets/Script/Enemy/EnemyPathfinding.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDir;
+    private Knockback Knockback;
+
+    private void Awake()
+    {
+        Knockback = GetComponent<Knockback>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Knockback.GettingKnockedBack) { return; }
+        rb.MovePosition(rb.position + moveDir * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    public void MoveTo(Vector2 targetPosition)
+    {
+        moveDir = targetPosition;
+    }
+}
