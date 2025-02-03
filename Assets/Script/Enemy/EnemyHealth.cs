60de8ce46975ca5e973f997a7306308c0192b04c
Kaiz Dawson <kaizdawson@gmail.com>
2025-02-03 21:52:52 +0700

    base

---
+++ Assets/Script/Enemy/EnemyHealth.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int startingHealth = 3;
+    [SerializeField] private GameObject deathVGXPrefab;
+    [SerializeField] private float knockBackThrust = 15f;
+    private int currentHealth;
+    private Knockback Knockback;
+    private Flash flash;
+
+    private void Awake()
+    {
+        flash = GetComponent<Flash>();
+        Knockback = GetComponent<Knockback>();
+    }
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Knockback.GetKnockedBack(PlayerController.Instance.transform, knockBackThrust);
+        StartCoroutine(flash.FlashRoutine());
+        StartCoroutine(CheckDetectDeathRoutine());
+    }
+
+    private IEnumerator CheckDetectDeathRoutine()
+    {
+        yield return new WaitForSeconds(flash.GetRestoreMatTime());
+        DetectDeath();
+    }
+    public void DetectDeath()
+    {
+        if (currentHealth <= 0)
+        {
+            Instantiate(deathVGXPrefab, transform.position,Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
