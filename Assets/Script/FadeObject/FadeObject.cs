dac0482d032a4de7f335183b6b0e151222d1d1af
nguyenvantoanpr359 <nguyenvantoanpr359@gmail.com>
2025-02-11 00:07:47 +0700

    Cập nhật code Unity

---
+++ Assets/Script/FadeObject/FadeObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FadeObject : MonoBehaviour
+{
+    private TilemapRenderer tilemapRenderer;
+    public float fadeAmount = 0.4f;
+    private float originalAlpha;
+
+    void Start()
+    {
+        tilemapRenderer = GetComponent<TilemapRenderer>();
+        originalAlpha = tilemapRenderer.material.color.a;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Color tempColor = tilemapRenderer.material.color;
+            tempColor.a = fadeAmount;
+            tilemapRenderer.material.color = tempColor;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Color tempColor = tilemapRenderer.material.color;
+            tempColor.a = originalAlpha;
+            tilemapRenderer.material.color = tempColor;
+        }
+    }
+}
